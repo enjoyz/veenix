c86a1b9a27da51dfb1fd4a15fd77e703e132c684
jayanthsilesh <sileshsu@usc.edu>
2012-12-22 04:54:22 -0800

    Stating VFS

--- kernel/main/kmain.c
+++ kernel/main/kmain.c
@@ -206,7 +206,7 @@ idleproc_run(int arg1, void *arg2)
         hard_shutdown();
         return NULL;
 }
- some changes
+ 
 /**
  * This function, called by the idle process (within 'idleproc_run'), creates the
  * process commonly refered to as the "init" process, which should have PID 1.
